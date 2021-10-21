078064a394ff77e5347081b82b552adc2af5fa2e
sipe-daniel <sipetchawoudanieljordan@yahoo.fr>
2021-10-21 13:51:08 +0200

    :construction: add some classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     theme: {
-        
+        extend: {
+            minHeight:  {
+                500: "500px",
+                650: "650px"
+            }
+        }
     }
 }
